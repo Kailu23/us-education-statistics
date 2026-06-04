2c5513784662e2906dc4d35f348cf8ff211ee5cd
Marin Valenčak <valencakmarin@gmail.com>
2026-06-04 19:38:48 +0200

    refactor(dataLoader): move logic out of function
        - move record saving logic into its own function
        - rename variable so they make more sense

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -2,28 +2,49 @@ import { appState } from "./state.js";
 import { CSV_TO_ABBR } from "./utils/mappings.js";
 import { METRICS, TOPO_URL, CSV_PATH } from "./utils/constants.js";
 
-export function processCSV(rows) {
+export function normalizeEducationData(rows) {
     rows.forEach((row) => {
         const abbr = CSV_TO_ABBR[row.STATE?.trim()?.toUpperCase()];
         if (!abbr) return;
         const year = +row.YEAR;
         if (year < 2000 || year > 2019) return;
 
-        const enroll = +row.ENROLL || null;
-        const total = +row.TOTAL_EXPENDITURE || null;
+        const enrollment = +row.ENROLL || null;
+        const spending = +row.TOTAL_EXPENDITURE || null;
         const math = +row.AVG_MATH_4_SCORE || null;
-        const read = +row.AVG_READING_4_SCORE || null;
+        const reading = +row.AVG_READING_4_SCORE || null;
 
-        if (!appState.eduData[abbr]) appState.eduData[abbr] = {};
-        appState.eduData[abbr][year] = {
-            spending_per_pupil:
-                enroll && total ? (total * 1000) / enroll : null,
-            AVG_MATH_4_SCORE: math,
-            AVG_READING_4_SCORE: read,
-            ENROLL: enroll,
-            state: abbr,
-        };
+        saveRecord(
+            abbr,
+            year,
+            {
+                spending,
+                math,
+                reading,
+                enrollment,
+            }
+        )
     });
+}
+
+function saveRecord(
+    abbr,
+    year,
+    {
+        spending,
+        math,
+        reading,
+        enrollment,
+    }
+) {
+    if (!appState.eduData[abbr]) appState.eduData[abbr] = {};
+    appState.eduData[abbr][year] = {
+        spending_per_pupil: enrollment && spending ? (spending * 1000) / enrollment : null,
+        AVG_MATH_4_SCORE: math,
+        AVG_READING_4_SCORE: reading,
+        ENROLL: enrollment,
+        state: abbr,
+    };
 }
 
 export function getValue(abbr, year, metric) {
@@ -37,5 +58,5 @@ export async function loadData() {
     ]);
 
     appState.topoData = topo;
-    processCSV(csv);
+    normalizeEducationData(csv);
 }
